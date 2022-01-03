c929ff96c6529aaa5dbccc45cd318265f01def0d
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-03 14:29:28 -0300

    Terminando a primeira sessão

--- src/components/Plans/index.jsx
+++ src/components/Plans/index.jsx
@@ -2,20 +2,30 @@ import React from "react";
 
 import AppImg from '../../assets/Notificacao.svg';
 
-import { PlansContainer } from './style.js';
+import { PlansContainer, BaixaApp } from './style.js';
 
 const PlansApp = () => {
   return(
     <PlansContainer>
       <div className="container">
-        <div className="baixaApp">
+        <BaixaApp className="baixaApp">
           <h1>Baixe nosso app para desfrutar mais</h1>
           <p>
             Amet in elementum nulla scelerisque dui, egestas at. Elit
             consectetur turpis elementum amet vitae et etiam nec. Varius
             volutpat hac adipiscing tincidunt pretium.
           </p>
-        </div>
+          <button>
+            <div className="content">
+              Google Play
+            </div>
+          </button>
+          <button>
+            <div className="content">
+              App Store
+            </div>
+          </button>
+        </BaixaApp>
         <img src={AppImg} alt="" />
       </div>
     </PlansContainer>
--- src/components/Plans/style.js
+++ src/components/Plans/style.js
@@ -5,10 +5,49 @@ import PlansImg from '../../assets/Bg-planos.png';
 export const PlansContainer = styled.section`
   height: 475px;
   background: url(${PlansImg});
+  margin-bottom: 4rem;
 
   .container {
     display: flex;
     align-items: center;
     justify-content: space-between;
   }
+`;
+
+
+export const BaixaApp = styled.div `
+
+
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+
+    margin-bottom: 1rem;
+  }
+  p {
+    width: 75%;
+    text-align: justify;
+    font-size: 1.1rem;
+    line-height: 26px;
+
+    color: #6E7275;
+  }
+
+  button {
+    width: 200px;
+    height: 60px;
+
+    margin-top: 2rem;
+    margin-right: 1.5rem;
+
+    border: 0;
+    border-radius: 0.25rem;
+
+    background: #293145;
+
+    font-family: Poppins;
+    font-weight: bold;
+    color: #fff;
+  }
+
 `;
