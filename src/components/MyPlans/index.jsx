39c57960d0c2cf1fd37f66bfa13654c20f65da2a
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-04 06:51:22 -0300

    Criando os itens

--- src/components/MyPlans/index.jsx
+++ src/components/MyPlans/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { MyPlansContent, Container } from './style.js';
+import { MyPlansContent, Container, Row } from './style.js';
 
 const MyPlans = () => {
   return (
@@ -9,6 +9,42 @@ const MyPlans = () => {
         <h1>Nossos Planos</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Donec lacinia mi quis euismod ultrices.</p>
+
+        <Row>
+          <div className="item">
+            <h2>Bronze</h2>
+            <h3><sup>R$</sup> 28 <sub>/mes</sub></h3>
+
+            <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. 
+              Donec lacinia mi quis euismod ultrices
+            </p>
+
+            <button>Assinar</button>
+          </div>
+          <div className="item">
+            <h2>Bronze</h2>
+            <h3><sup>R$</sup> 28 <sub>/mes</sub></h3>
+
+            <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. 
+              Donec lacinia mi quis euismod ultrices
+            </p>
+
+            <button>Assinar</button>
+          </div>
+          <div className="item">
+            <h2>Bronze</h2>
+            <h3><sup>R$</sup> 28 <sub>/mes</sub></h3>
+
+            <p>Lorem ipsum dolor sit amet, 
+              consectetur adipiscing elit. 
+              Donec lacinia mi quis euismod ultrices
+            </p>
+
+            <button>Assinar</button>
+          </div>
+        </Row>
       </Container>
     </MyPlansContent>
   )
--- src/components/MyPlans/style.js
+++ src/components/MyPlans/style.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const MyPlansContent = styled.section `
-  height: 70vh;
+  padding-top: 3rem;
+  padding-bottom: 4rem;
+  /* height: 70vh; */
 `;
 
 
 export const Container = styled.div `
-  padding-top: 2rem;
   text-align: center;
 
   h1 {
@@ -14,6 +15,61 @@ export const Container = styled.div `
     font-size: 2.2rem;
 
     color: #293145;
+    margin-bottom: 1rem;
   }
 
+  > p {
+    width: 600px;
+    margin: 0 auto;
+    text-align: center;
+  }
+`;
+
+
+export const Row = styled.div `
+  display: flex;
+  justify-content: space-between;
+  margin-top: 2rem;
+
+  .item {
+    width: 32%;
+    height: 60vh;
+    background: #EFF8FF;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+
+    border-radius: 0.25rem;
+
+    h2 {
+      font-weight: bold;
+      margin-bottom: 1.5rem;
+    }
+
+    h3 {
+      font-size: 2rem;
+      font-weight: bold;
+      margin-bottom: 2rem;
+
+      sup,
+      sub {
+        font-size: 1rem;
+      }
+    }
+
+    p {
+      width: 300px;
+      font-size: 1rem;
+      margin-bottom:3rem;
+    }
+
+    button {
+      width: 220px;
+      height: 40px;
+      background: #0F9AFE;
+    }
+  }
 `;
