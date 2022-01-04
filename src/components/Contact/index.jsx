c5ebf0b9a668c81aa5593e5cc1509e95fb075320
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2022-01-04 07:31:38 -0300

    Trabalhando na parte de contacts

---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,13 @@
+import React from './react';
+
+import { SectionContacts, Container } from './style.js';
+
+const Contact = () => {
+  return (
+    <SectionContacts>
+      <Container className="container"></Container>
+    </SectionContacts>
+  )
+}
+
+export { Contact };
---
+++ src/components/Contact/style.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const SectionContacts = styled.section ``;
+
+
+export const Container = styled.div ``;
